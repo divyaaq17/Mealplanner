9bccee8b33bb5348beb3ca54ad306fa526b77394
Divyaaq17 <divya.aqueleen@gmail.com>
2026-08-12 22:33:09 +0530

    Use MySQL connection pool

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+/* const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -17,4 +17,32 @@ connection.connect((err) => {
     console.log("Database connected successfully!");
 });
 
-module.exports = connection;
+module.exports = connection; */
+
+
+require("dotenv").config();
+
+const mysql = require("mysql2");
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+db.getConnection((err, connection) => {
+    if (err) {
+        console.log("Database connection failed:");
+        console.log(err);
+    } else {
+        console.log("Connected to Railway MySQL");
+        connection.release();
+    }
+});
+
+module.exports = db;
